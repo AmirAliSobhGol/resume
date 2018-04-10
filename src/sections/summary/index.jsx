d5a566ecacaac2acae877d3634e58fe2f23ae9e7
amiralisobhgol <amiralisobhgol@gmail.com>
2018-04-10 15:16:27 +0430

    remove unused code

--- src/sections/summary/index.jsx
+++ src/sections/summary/index.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import {differenceInCalendarYears, differenceInCalendarMonths} from 'date-fns';
+import {differenceInCalendarYears} from 'date-fns';
 
-
-// const getFormattedMonth = (months) => {
-//   const monthsPercent = (months % 12) / 10;
-//   console.log(months, months%12,monthsPercent)
-//   if (monthsPercent >= 0.75) {
-//     return '.75'
-//   } else if ( monthsPercent >= 0.5) {
-//     return '.5'
-//   } else if ( monthsPercent >= 0.25) {
-//     return '.25'
-//   } else {
-//     return ''
-//   }
-// }
 const getFormattedWorkExprience = (startDate) => {
   const workingYears = differenceInCalendarYears(new Date(), startDate);
-  const workingMonths = differenceInCalendarMonths(new Date(), startDate);
   return workingYears
 }
 
